005d33ef54817f99e87139183a167d52315811b0
Aya <126961847+AyaElbek@users.noreply.github.com>
2024-09-30 15:02:12 +0200

    Add files via upload

---
+++ DataScienceEcosystem-2.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Data Science Tools and Ecosystem
+
+
+# In[2]:
+
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+
+# In[19]:
+
+
+# Create a Markdown cell listing popular languages
+languages = ["Python", "R", "SQL", "Java", "Scala"]
+
+# Create the ordered list
+languages_markdown = "Some of the popular languages that Data Scientists use are:\n" +                      "\n".join([f"{i + 1}. {lang}" for i, lang in enumerate(languages)])
+print(languages_markdown)
+
+
+# In[11]:
+
+
+### Below are a few examples of evaluating arithmetic expressions in Python.
+
+
+# In[20]:
+
+
+# Create a Markdown cell listing popular libraries
+libraries = ["Pandas", "NumPy", "Matplotlib", "Scikit-learn"]
+
+# Create the ordered list
+libraries_markdown = "Some of the commonly used libraries used by Data Scientists include:\n" +                       "\n".join([f"{i + 1}. {lib}" for i, lib in enumerate(libraries)])
+print(libraries_markdown)
+
+
+# In[23]:
+
+
+#| Data Science Tools      |
+#|-------------------------|
+#| Jupyter Notebook        |
+#| RStudio                 |
+#| Apache Zeppelin         |
+
+
+# In[24]:
+
+
+# Below are a few examples of evaluating arithmetic expressions in Python
+
+
+# In[12]:
+
+
+# This is a simple arithmetic expression to multiply then add integers
+result = (3 * 4) + 5
+result
+
+
+# In[13]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+hours = 200 / 60
+hours
+
+
+# In[25]:
+
+
+# Create a Markdown cell listing objectives
+objectives_markdown = "**Objectives:**\n" +                        "- List popular languages for Data Science.\n" +                        "- List commonly used libraries.\n" +                        "- Create tables for Data Science tools.\n" +                        "- Perform basic arithmetic operations in Python."
+print(objectives_markdown)
+
+
+# In[26]:
+
+
+# Create a Markdown cell for Author details
+author_markdown = "## Author Aidai ELbekova"
+print(author_markdown)
+
+
+# In[ ]:
+
+
+
+
